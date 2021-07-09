2c6e1dab47746a23b37e7b47173c1d0ea1843646
boedhack99 <boedhack@gmail.com>
2021-07-09 14:44:30 +0700

    Remove sudo (1)

--- setup/make.sh
+++ setup/make.sh
@@ -6,6 +6,6 @@ tar xvzf /tmp/make-"${1:?}".tar.gz
 cd /tmp/make-"${1:?}" || exit 1
 ./configure
 bash ./build.sh
-sudo install ./make /usr/local/bin/make
+install ./make /usr/local/bin/make
 cd - || exit 1
 rm -rf /tmp/make-"${1:?}"{,.tar.gz}
