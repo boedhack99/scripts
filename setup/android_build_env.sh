7b77c9f4b655a4abddb4d71a914a9d39255e3850
boedhack99 <boedhack@gmail.com>
2021-07-14 14:31:34 +0700

    Setting python env

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -44,7 +44,7 @@ DEBIAN_FRONTEND=noninteractive \
     libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev \
     libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils '^lzma.*' lzop \
     maven ncftp ncurses-dev patch patchelf pkg-config pngcrush \
-    pngquant python2.7 python-all-dev re2c schedtool squashfs-tools subversion systemd \
+    pngquant python2.7 python-all-dev python-is-python3 re2c schedtool squashfs-tools subversion systemd \
     texinfo unzip w3m xsltproc zip zlib1g-dev lzip \
     libxml-simple-perl apt-utils gh \
     ${PACKAGES} -y
@@ -54,6 +54,7 @@ sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://r
 sudo chmod 644 /etc/udev/rules.d/51-android.rules
 sudo chown root /etc/udev/rules.d/51-android.rules
 sudo systemctl restart udev
+sudo ln -s python3 /usr/bin/python
 
 if [[ "$(command -v make)" ]]; then
     makeversion="$(make -v | head -1 | awk '{print $3}')"
