bbb367050bf4e905140424c40667a82ac8099aed
boedhack99 <boedhack@gmail.com>
2021-07-14 14:26:35 +0700

    back to sudo

--- setup/make.sh
+++ setup/make.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 cd /tmp || exit 1
-apt install axel -y
+sudo apt install axel -y
 axel -a -n 10 https://ftp.gnu.org/gnu/make/make-"${1:?}".tar.gz
 tar xvzf /tmp/make-"${1:?}".tar.gz
 cd /tmp/make-"${1:?}" || exit 1
 ./configure
 bash ./build.sh
-install ./make /usr/local/bin/make
+sudo install ./make /usr/local/bin/make
 cd - || exit 1
 rm -rf /tmp/make-"${1:?}"{,.tar.gz}
